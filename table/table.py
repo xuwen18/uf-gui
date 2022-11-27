9f2cc253a633ab702ed9306bb8dc1b6cd0cfbf64
xuwen18 <117784280+xuwen18@users.noreply.github.com>
2022-11-26 23:04:26 -0800

    Functional buttons

--- table/table.py
+++ table/table.py
@@ -2,12 +2,18 @@ from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
+import os
 import sys
+import csv
 from typing import List
 
 class EditPopup(QDialog):
-    def __init__(self, parent=None):
-        super(EditPopup, self).__init__(parent)
+    _edit_row = -1
+    _delete_reject = False
+    def __init__(self, setItem, removeRow, parent=None):
+        super().__init__(parent)
+        self._removeRow = removeRow
+        self._setItem = setItem
         self.resize(400, 200)
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
         self.setWindowTitle('Edit')
@@ -57,38 +63,74 @@ class EditPopup(QDialog):
         self.radioButton_4.setText('4')
         self.gridLayout.addWidget(self.radioButton_4, 4, 1, 1, 1)
 
-        self.doubleSpinBox = QDoubleSpinBox(self)
-        self.doubleSpinBox.setObjectName(u"doubleSpinBox")
-        self.gridLayout.addWidget(self.doubleSpinBox, 6, 1, 1, 1)
+        self.flow_rate = QDoubleSpinBox(self)
+        self.flow_rate.setObjectName(u"flow_rate")
+        self.flow_rate.setSingleStep(0.01)
+        self.gridLayout.addWidget(self.flow_rate, 6, 1, 1, 1)
 
-        self.spinBox = QSpinBox(self)
-        self.spinBox.setObjectName(u"spinBox")
-        self.gridLayout.addWidget(self.spinBox, 7, 1, 1, 1)
+        self.time = QSpinBox(self)
+        self.time.setObjectName(u"time")
+        self.gridLayout.addWidget(self.time, 7, 1, 1, 1)
 
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
 
         self.buttonBox = QDialogButtonBox(self)
         self.buttonBox.setObjectName(u"buttonBox")
         self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
+        self.buttonBox.setStandardButtons(
+            QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok
+        )
 
         self.gridLayout_2.addWidget(self.buttonBox, 1, 0, 1, 1)
 
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
 
+    def popup(self, edit_row: int, delete_reject: bool):
+        self._edit_row = edit_row
+        self._delete_reject = delete_reject
+        self.show()
 
-def _lastOf(x: List[QModelIndex]):
-    return x[len(x)-1].row()
+    def validEditRow(self):
+        assert self._edit_row != -1 #TODO: handle properly
+        return self._edit_row
+    
+    def accept(self) -> None:
+        if self.radioButton_1.isChecked():
+            r = '1'
+        elif self.radioButton_2.isChecked():
+            r = '2'
+        elif self.radioButton_3.isChecked():
+            r = '3'
+        elif self.radioButton_4.isChecked():
+            r = '4'
+        else:
+            r = 'None'
+        self._setItem(self.validEditRow(), 0, QTableWidgetItem(r))
+        f = f'{self.flow_rate.value():.2f}'
+        self._setItem(self.validEditRow(), 1, QTableWidgetItem(f))
+        t = str(self.time.value())
+        self._setItem(self.validEditRow(), 2, QTableWidgetItem(t))
+       
+        self._edit_row = -1
+        self._delete_reject = False
+        return super().accept()
+
+    def reject(self) -> None:
+        if self._delete_reject:
+            self._removeRow(self.validEditRow())
+        
+        self._edit_row = -1
+        self._delete_reject = False
+        return super().reject()
 
 class TableForm(QFrame):
     
     def __init__(self, parent=None):
-        super(TableForm, self).__init__(parent)
+        super().__init__(parent)
 
         self.resize(500, 300)
-
-        self.editPopup = EditPopup()
+        self.setAcceptDrops(True)
 
         self.gridLayout = QGridLayout(self)
         self.gridLayout.setObjectName(u"gridLayout")
@@ -99,19 +141,22 @@ class TableForm(QFrame):
         self.table = QTableWidget(self)
         if (self.table.columnCount() < 3):
             self.table.setColumnCount(3)
-        __qtablewidgetitem = QTableWidgetItem('Reagent')
-        self.table.setHorizontalHeaderItem(0, __qtablewidgetitem)
-        __qtablewidgetitem1 = QTableWidgetItem('Flow Rate')
-        self.table.setHorizontalHeaderItem(1, __qtablewidgetitem1)
-        __qtablewidgetitem2 = QTableWidgetItem('Time')
-        self.table.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        self.table.setHorizontalHeaderItem(0, QTableWidgetItem('Reagent'))
+        self.table.setHorizontalHeaderItem(1, QTableWidgetItem('Flow Rate'))
+        self.table.setHorizontalHeaderItem(2, QTableWidgetItem('Time'))
         self.table.setObjectName(u"table")
         self.table.horizontalHeader().setMinimumSectionSize(100)
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         self.table.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
-        # self.table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
-
+        self.table.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
+        self.table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.table.selectionModel().selectionChanged.connect(self.onSelectionChange)
         self.horizontalLayout.addWidget(self.table)
+
+        self.editPopup = EditPopup(
+            setItem=self.table.setItem, 
+            removeRow=self.table.removeRow
+        )
 
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
@@ -119,6 +164,7 @@ class TableForm(QFrame):
         self.buttonLoad = QPushButton(self)
         self.buttonLoad.setObjectName(u"buttonLoad")
         self.buttonLoad.setText('&Load')
+        self.buttonLoad.clicked.connect(self.loadFile)
         self.verticalLayout.addWidget(self.buttonLoad)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
@@ -133,11 +179,13 @@ class TableForm(QFrame):
         self.buttonEdit = QPushButton(self)
         self.buttonEdit.setObjectName(u"buttonEdit")
         self.buttonEdit.setText('&Edit')
+        self.buttonEdit.clicked.connect(self.editRow)
         self.verticalLayout.addWidget(self.buttonEdit)
 
         self.buttonDelete = QPushButton(self)
         self.buttonDelete.setObjectName(u"buttonDelete")
         self.buttonDelete.setText('&Delete')
+        self.buttonDelete.clicked.connect(self.deleteRow)
         self.verticalLayout.addWidget(self.buttonDelete)
 
         self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
@@ -146,39 +194,140 @@ class TableForm(QFrame):
         self.buttonUp = QPushButton(self)
         self.buttonUp.setObjectName(u"buttonUp")
         self.buttonUp.setText('&Up')
+        self.buttonUp.clicked.connect(self.upRow)
         self.verticalLayout.addWidget(self.buttonUp)
 
         self.buttonDown = QPushButton(self)
         self.buttonDown.setObjectName(u"buttonDown")
         self.buttonDown.setText('D&own')
+        self.buttonDown.clicked.connect(self.downRow)
         self.verticalLayout.addWidget(self.buttonDown)
 
         self.horizontalLayout.addLayout(self.verticalLayout)
 
         self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
 
-    def setButtons(self, b: bool):
-        self.buttonLoad.setEnabled(b)
-        self.buttonNew.setEnabled(b)
+        self.setButtonsSelection(False)
+
+    def setButtonsSelection(self, b: bool):
         self.buttonEdit.setEnabled(b)
         self.buttonDelete.setEnabled(b)
         self.buttonUp.setEnabled(b)
         self.buttonDown.setEnabled(b)
-    
-    def setButtonsEnabled(self):
-        self.setButtons(True)
 
-    def setButtonsDisabled(self):
-        self.setButtons(False)
+    def setButtons(self, b: bool):
+        self.buttonLoad.setEnabled(b)
+        self.buttonNew.setEnabled(b)
+        self.setButtonsSelection(b)
+    
+    def onSelectionChange(self, selected: QItemSelection, deselected: QItemSelection):
+        idx = selected.indexes()
+        self.setButtonsSelection(len(idx) > 0)
 
     def newRow(self):
         idx = self.table.selectionModel().selectedRows()
         if len(idx) > 0:
-            self.table.insertRow(1+_lastOf(idx))
+            ed = 1+idx[0].row()
         else:
-            self.table.insertRow(self.table.rowCount())
-        self.editPopup.show()
+            ed = self.table.rowCount()
+        self.table.insertRow(ed)
+        self.editPopup.flow_rate.setValue(0.0)
+        self.editPopup.time.setValue(0)
+        self.editPopup.popup(ed, True)
     
+    def editRow(self):
+        idx = self.table.selectionModel().selectedRows()
+        if len(idx) > 0:
+            ed = idx[0].row()
+            r = self.table.item(ed, 0).text()
+            if r == '1':
+                self.editPopup.radioButton_1.setChecked(True)
+            elif r == '2':
+                self.editPopup.radioButton_2.setChecked(True)
+            elif r == '3':
+                self.editPopup.radioButton_3.setChecked(True)
+            elif r == '4':
+                self.editPopup.radioButton_4.setChecked(True)
+            else:
+                self.editPopup.radioButton_None.setChecked(True)
+            f = float(self.table.item(ed, 1).text())
+            self.editPopup.flow_rate.setValue(f)
+            t = int(self.table.item(ed, 2).text())
+            self.editPopup.time.setValue(t)
+            self.editPopup.popup(ed, False)
+    
+    def deleteRow(self):
+        idx = self.table.selectionModel().selectedRows()
+        if len(idx) > 0:
+            self.table.removeRow(idx[0].row())
+            idx = self.table.selectionModel().selectedRows()
+            self.table.selectRow(idx[0].row())
+
+    def moveRow(self, row_upper: int):
+        if row_upper >= 0 and row_upper+1 < self.table.rowCount():
+            for i in [0, 1, 2]:
+                a = self.table.takeItem(row_upper, i)
+                b = self.table.takeItem(row_upper+1, i)
+                self.table.setItem(row_upper+1, i, a)
+                self.table.setItem(row_upper, i, b)
+
+    def upRow(self):
+        idx = self.table.selectionModel().selectedRows()
+        if len(idx) > 0:
+            ed = idx[0].row()
+            self.moveRow(ed-1)
+            if ed > 0 and ed < self.table.rowCount():
+                self.table.selectRow(ed-1)
+            else:
+                self.table.selectRow(ed)
+    
+    def downRow(self):
+        idx = self.table.selectionModel().selectedRows()
+        if len(idx) > 0:
+            ed = idx[0].row()
+            self.moveRow(ed)
+            if ed+1 > 0 and ed+1 < self.table.rowCount():
+                self.table.selectRow(ed+1)
+            else:
+                self.table.selectRow(ed)
+
+    def loadFile(self):
+        file, _ = QFileDialog.getOpenFileName(self, "Open File",
+            "", "CSV File (*.csv)"
+        )
+        if len(file) > 0:
+            self.loadCSV(file)
+
+    def dragEnterEvent(self, event: QDragEnterEvent):
+        if event.mimeData().hasText() and _isCSV(event.mimeData().text()):
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event: QDropEvent):
+        url: str = event.mimeData().text()
+        self.loadCSV(_noPre('file:///', url))
+    
+    def loadCSV(self, name: str):
+        self.table.setRowCount(0)
+        with open(name, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                r = self.table.rowCount()
+                self.table.insertRow(r)
+                assert len(row) == 3
+                for i in [0, 1, 2]:
+                    self.table.setItem(r, i, QTableWidgetItem(row[i]))
+
+
+def _isCSV(url: str) -> bool:
+    ext = os.path.splitext(url)[1]
+    return ext == '.csv'
+
+def _noPre(prefix: str, text: str) -> str:
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
 
 # for testing
 def main():
